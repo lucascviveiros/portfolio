21e73134d5bc2d6643a9c69259bbac0fe9db6d21
LV <viveiros.lucas@hotmail.com>
2023-05-18 15:12:51 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 const header = document.querySelector(".navbar-dark");
-console.log(header);
-
-window.onscroll = function() {
-  window.scrollY >= 100
-    ? header.classList.add("navbarDark")
-    : header.classList.remove("navbarDark");
+console.log(header), window.onscroll = function() {
+    window.scrollY >= 100 ? header.classList.add("navbarDark") : header.classList.remove("navbarDark")
 };
-
-const navLinks = document.querySelectorAll(".nav-item");
-const menuToggle = document.getElementById("navbarSupportedContent");
-
-navLinks.forEach((e) => {
-  e.addEventListener("click", () => {
-    // Add a delay before scrolling to the target
-    setTimeout(() => {
-      new bootstrap.Collapse(menuToggle).toggle();
-    }, 2000); // Adjust the delay time (in milliseconds) to control the scroll speed
-  });
-});
+const navLinks = document.querySelectorAll(".nav-item"),
+    menuToggle = document.getElementById("navbarSupportedContent");
+navLinks.forEach((e => {
+    e.addEventListener("click", (() => {
+        new bootstrap.Collapse(menuToggle).toggle()
+    }))
+}));
